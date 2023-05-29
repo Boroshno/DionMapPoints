191160d03a5472b4202b3ff432fe41f5973f94cb
Dmytro Lytvynenko <dmytro.lytvynenko@fortius-network.com>
2023-05-29 16:10:11 +0300

    stable version 0.1

---
+++ places.js
@@ -0,0 +1,39 @@
+window.places1 = [
+  {
+    lat: 51.5,
+    lng: -0.09,
+    name: 'Place1',
+    description: 'This is the first place in List 1.',
+    img1: 'https://i.gyazo.com/c88ad334554f8a9e5631803df2616f9b.png',
+    img2: 'https://i.gyazo.com/bdd50741141a23141622a4d3b78bf83b.png'
+  },
+  {
+    lat: 51.51,
+    lng: -0.1,
+    name: 'Place2',
+    description: 'This is the second place in List 1.',
+    img1: 'https://i.gyazo.com/c88ad334554f8a9e5631803df2616f9b.png',
+    img2: 'https://i.gyazo.com/bdd50741141a23141622a4d3b78bf83b.png'
+  },
+  // More places...
+];
+
+window.places2 = [
+  {
+    lat: 51.6,
+    lng: -0.11,
+    name: 'Place3',
+    description: 'This is the first place in List 2.',
+    img1: 'https://i.gyazo.com/c88ad334554f8a9e5631803df2616f9b.png',
+    img2: 'https://i.gyazo.com/bdd50741141a23141622a4d3b78bf83b.png'
+  },
+  {
+    lat: 51.61,
+    lng: -0.12,
+    name: 'Place4',
+    description: 'This is the second place in List 2.',
+    img1: 'https://i.gyazo.com/c88ad334554f8a9e5631803df2616f9b.png',
+    img2: 'https://i.gyazo.com/bdd50741141a23141622a4d3b78bf83b.png'
+  },
+  // More places...
+];
