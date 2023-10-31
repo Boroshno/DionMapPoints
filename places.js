346e727d857ac36cf4384aadf66c74f053158f65
Dmytro Lytvynenko <dmytro.lytvynenko@fortius-network.com>
2023-10-31 16:17:27 +0200

    New points

--- places.js
+++ places.js
@@ -10,12 +10,34 @@ window.places1 = [
     img2: 'https://i.gyazo.com/3a83cd9e0512a10112b4f77fdbe044d4.png'
   },
   {
-    lat: 51.51,
-    lng: -0.1,
-    name: 'Place2',
-    description: 'This is the second place in List 1.',
-    img1: 'https://i.gyazo.com/c88ad334554f8a9e5631803df2616f9b.png',
-    img2: 'https://i.gyazo.com/bdd50741141a23141622a4d3b78bf83b.png'
+    lat: 48.293003,
+    lng: 25.931097,
+    name: 'UniversytetskaSt22Chernivtsi',
+    description: 'Universytetska St, 22, Chernivtsi. Колишні склади монополії з продажу тютюну в Чернівцях.',
+    yearbefore: '1930',
+    yearafter: '2023',
+    img1: 'https://admin.cvnews.cv.ua/uploads/600x400/2019/05/18/06a9662413f8a13f7155e2b57f8d7296.png',
+    img2: 'https://scontent.fhrk6-1.fna.fbcdn.net/v/t39.30808-6/394622018_234930819591240_3193810572796459717_n.jpg?stp=cp6_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ghuSGMQNKjAAX-b67Nl&_nc_ht=scontent.fhrk6-1.fna&oh=00_AfDiMwb7PVppbeeTWF18ZYBPlkdyHfibIceCh08Wa3fOlg&oe=65468D50'
+  },
+  {
+    lat: 37.971293,
+    lng: 23.719226,
+    name: 'AcropolisofAthens',
+    description: 'Acropolis of Athens',
+    yearbefore: '1851',
+    yearafter: '2021',
+    img1: 'https://i.gyazo.com/a832365d94365cfeca37abcf709d91dc.png',
+    img2: 'https://i.gyazo.com/84db7ebc11ec2da756924255c92f91dc.png'
+  },
+  {
+    lat: 49.454383,
+    lng: 32.056088,
+    name: 'UzvizSadovuyCherkasy',
+    description: 'Узвіз Судовий в Черкасах',
+    yearbefore: '1930',
+    yearafter: '2022',
+    img1: 'https://i.gyazo.com/d1a1e31a766053885c8d4dc871b29580.png',
+    img2: 'https://i.gyazo.com/0238de2ed78d8026ba9b64397be48dd0.png'
   },
   // More places...
 ];
